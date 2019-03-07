e142d2c4c2084aa3ce47948d7d557abf1e98e25f
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-07 16:47:24 +0000

    Found Waldo at index...

---
+++ findWaldo.js
@@ -0,0 +1,14 @@
+function findWaldo(arr, found) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found(i);   // execute callback
+      }
+    }
+  }
+  
+  function actionWhenFound(i) {
+    console.log("Found Waldo at index", i + '!');
+  }
+  
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+  
