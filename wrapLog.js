f8fd6c4a1f09d7441ed6da048d143af53d42ea7e
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-08 00:37:27 +0000

    solved!

--- wrapLog.js
+++ wrapLog.js
@@ -1,13 +1,12 @@
-var wrapLog = function (callback, name) {
-  
-    return function () {
-        var input = Object.values(arguments);
-        var formattedInput = input.join(', ');
-        console.log(name, '(' + formattedInput + ')', '=>', callback(...input));
-    } 
+var wrapLog = function(callback, name) {
+  return function() {
+    var input = Object.values(arguments);
+    var formattedInput = input.join(", ");
+    console.log(name, "(" + formattedInput + ")", "=>", callback(...input));
+  };
 };
 
-var area = function (x, y) {
+var area = function(x, y) {
   return x * y;
 };
 var logArea = wrapLog(area, "area");
@@ -15,7 +14,7 @@ var logArea = wrapLog(area, "area");
 logArea(5, 3); // area(5, 3) => 15
 logArea(3, 2); // area(3, 2) => 6
 
-var volume = function (x, y, z, ) {
+var volume = function(x, y, z) {
   return x * y * z;
 };
 var logVolume = wrapLog(volume, "volume");
